68c717bea67ec8c9200596c902a71cefa751c277
Ram <ramtin63@gmail.com>
2026-01-04 16:49:06 -0800

    auto-creating website feature

--- app/lib/supabase.server.ts
+++ app/lib/supabase.server.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseServer = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL1!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
