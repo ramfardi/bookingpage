0703132d659f0a3190da585352bf1fc7557e0e5a
Ram <ramtin63@gmail.com>
2026-01-07 22:36:58 -0800

    edit page feature

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation";
 export default function HomePage({
   activeCustomer,
 }: {
-  activeCustomer: CustomerConfig;
+  activeCustomer?: CustomerConfig;
 }) {
   const router = useRouter();
 
