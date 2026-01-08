babd3298f4fdcc1a3ecb2cc201ebb5c86ca8e70a
Ram <ramtin63@gmail.com>
2026-01-07 21:20:36 -0800

    payment

--- app/api/stripe/session/route.ts
+++ app/api/stripe/session/route.ts
@@ -1,9 +1,7 @@
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
