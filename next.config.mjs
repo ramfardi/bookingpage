d26400d9bf095b987dbd651ca0186e6b4d430664
Ram <ramtin63@gmail.com>
2026-01-30 18:34:38 -0800

    updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.simplebookme.com" }],
+        destination: "https://simplebookme.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
